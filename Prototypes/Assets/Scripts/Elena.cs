978a984a4841cdcceaa678f0ac04079e24145aee
Ibrahim Aslan <i.aslan2020@gtu.edu.tr>
2022-09-21 13:01:54 +0300

    Added some mechanics
    Fixed some issues of dash, and created slide.

--- Prototypes/Assets/Scripts/Elena.cs
+++ Prototypes/Assets/Scripts/Elena.cs
@@ -10,17 +10,29 @@ public class Elena : MonoBehaviour
     
     [Header("Jump")]
     [SerializeField] float jumpPower;
+    bool onGround;
 
     [Header("Dash")]
     [SerializeField] float dashPower;
     [SerializeField] float dashTime = 1f;
-    bool isDashing;
+    [SerializeField] float dashCooldown = 1.5f;
+    [SerializeField] bool canDash;
+    [SerializeField] bool isDashing;
+    float dashCooldownCounter;
+
+    [Header("Slide")]
+    [SerializeField] float slidePower;
+    [SerializeField] float slideTime;
+    [SerializeField] float slideCooldown = 1.5f;
+    [SerializeField] bool canSlide;
+    [SerializeField] bool isSliding;
+    float slideCooldownCounter;
 
     [Header("Attack")]
+    [SerializeField] float movementFreezeTimer;
+    [SerializeField] float attackLeapPower;
+    [SerializeField] float attackLeapTime;
     bool isAttacking;
-    float movementFreezeTimer;
-    public float attackLeapPower;
-    public float attackLeapTime;
 
     Rigidbody2D rb;
     Animator anim;
@@ -35,8 +47,26 @@ public class Elena : MonoBehaviour
         feetCol = GetComponent<BoxCollider2D>();
     }
 
+    private void Start()
+    {
+        canDash = true;
+        canSlide = true;
+    }
+
     private void Update()
     {
+        if (!canDash)
+        {
+            dashCooldownCounter -= Time.deltaTime;
+            if (dashCooldownCounter <= 0) canDash = true;
+        }
+        else if (!canSlide)
+        {
+            slideCooldownCounter -= Time.deltaTime;
+            if (slideCooldownCounter <= 0) canSlide = true;
+        }
+
+
         anim.SetBool("isAttacking", isAttacking);
         if (isAttacking)
         {
@@ -47,6 +77,8 @@ public class Elena : MonoBehaviour
             }
             else isAttacking = false;
         }
+
+
         Movement();
         Flip();
         anim.SetBool("Fall", IsFalling());
@@ -57,12 +89,6 @@ public class Elena : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-    }
-
-    void OnSecondAttack(InputValue value)
-    {
-        anim.SetTrigger("SecondAttack");
-        StartCoroutine(AttackLeapCoroutine());
     }
 
     void OnAttack(InputValue value)
@@ -86,11 +112,22 @@ public class Elena : MonoBehaviour
 
     void OnDash(InputValue value)
     {
-        if (value.isPressed && IsMoving())
+        canDash = IsMoving() && onGround && !isSliding && dashCooldownCounter <= 0;
+        if (value.isPressed && canDash)
         {
             StartCoroutine(DashCoroutine());
         }
     }
+
+    void OnSlide(InputValue value)
+    {
+        canSlide = IsMoving() && onGround && !isDashing && slideCooldownCounter <= 0;
+        if (value.isPressed && canSlide) 
+        {
+            StartCoroutine(SlideCoroutine());
+        }
+    }
+
     #endregion
 
     void Movement()
@@ -111,13 +148,16 @@ public class Elena : MonoBehaviour
 
     bool IsFalling()
     {
-        bool onGround = feetCol.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        onGround = feetCol.IsTouchingLayers(LayerMask.GetMask("Ground"));
         return !onGround && rb.velocity.y <= Mathf.Epsilon;
     }
 
     IEnumerator DashCoroutine()
     {
         isDashing = true;
+        canDash = false;
+        dashCooldownCounter = dashCooldown;
+
         anim.SetTrigger("Dash");
         Vector2 dashSpeed = transform.localScale.x * dashPower * transform.right;
 
@@ -130,10 +170,28 @@ public class Elena : MonoBehaviour
         isDashing = false;
     }
 
+    IEnumerator SlideCoroutine()
+    {
+        isSliding = true;
+        canSlide = false;
+        slideCooldownCounter = slideCooldown;
+        
+        anim.SetTrigger("Slide");
+        Vector2 slideSpeed = transform.localScale.x * slidePower * transform.right;
+
+        float slideStartTime = Time.time;
+        while (Time.time < slideStartTime + slideTime)
+        {
+            transform.Translate(slideSpeed * Time.deltaTime);
+            yield return null;
+        }
+        isSliding = false;
+    }
+
     IEnumerator AttackLeapCoroutine()
     {
         isAttacking = true;
-        movementFreezeTimer = .75f;
+        movementFreezeTimer = .4f;
         Vector2 attackLeap = transform.localScale.x * attackLeapPower * transform.right;
 
         float attackLeapStartTime = Time.time;
